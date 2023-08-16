cd832fb65a8abed201735cda24273476ff926827
Kele <ebiritg@gmail.com>
2023-08-16 08:10:54 +0600

    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

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    
+    biggest = my_list[0]
+    for i in my_list:
+        if biggest >= i:
+            pass
+        else:
+            biggest = i
+    return biggest
+
