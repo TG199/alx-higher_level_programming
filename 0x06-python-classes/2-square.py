1ebec30b93d51a3ddaf47590b6b3060590b1bbd0
Kelechi <ebirikele@gmail.com>
2023-08-30 07:06:43 +0600

    Write a class Square that defines a square by: (based on 1-square.py)

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -17,3 +17,11 @@ class Square:
             raise ValueError("size must be >=  0")
         else:
             self.__size = size
+
+    def area(self):
+        """Calculate the area of the square.
+
+            Returns:
+                int: the area of the square
+        """
+        return self.__size ** 2
