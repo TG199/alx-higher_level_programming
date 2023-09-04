af7f2124ff97ca7edec3f80def2b236d442834d0
Kelechi <ebirikele@gmail.com>
2023-09-05 03:49:15 +0600

    Write a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,59 +10,59 @@ class Rectangle:
         self.__width = width
         self.__height = height
 
-        @property
-        def width(self):
-            """ Method for retrieving width atrr
+    @property
+    def width(self):
+        """ Method for retrieving width atrr
 
-            Returns:
-                width of the rectangle
+        Returns:
+            width of the rectangle
 
-            """
-            return self.__width
+        """
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """Method for setting a width
+    @width.setter
+    def width(self, value):
+        """Method for setting a width
 
-            Args:
-                value: width
+        Args:
+            value: width
 
-            Raises:
-                TypeError: if width is not an integer
-                ValueError: if width is < 0
+        Raises:
+            TypeError: if width is not an integer
+            ValueError: if width is < 0
 
-            """
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-        @property
-        def height(self):
-            """Method for retrieving height attr
+    @property
+    def height(self):
+        """Method for retrieving height attr
 
-            Returns:
-                height of the rectangle
+        Returns:
+            height of the rectangle
 
-            """
-            return self.__height
+        """
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """ Method for setting a height atrr
+    @height.setter
+    def height(self, value):
+        """ Method for setting a height atrr
 
-            Args:
-                value: height
+        Args:
+            value: height
 
-            Raises:
-                TypeError: if height is not an integer
-                ValueError: if height < 0
+        Raises:
+            TypeError: if height is not an integer
+            ValueError: if height < 0
 
 
-            """
-            if not isinstance(value, int):
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            self.__height = value
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
