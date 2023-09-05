19f7acd30fac01cddd748f9ea01f1ff745ad227a
Kelechi <ebirikele@gmail.com>
2023-09-05 07:36:56 +0600

    Write a class Rectangle that defines a rectangle by: (based on 4-rectangle.py)

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -104,4 +104,4 @@ class Rectangle:
     def __del__(self):
         """Method to handle deletion of an instance
         """
-        print("Bye Rectangle{}".format("..."))
+        print("Bye rectangle...")
