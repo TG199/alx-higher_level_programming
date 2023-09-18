6597dc04adf0b31785db630537220c277a034519
Kelechi <ebirikele@gmail.com>
2023-09-18 22:03:40 +0100

    Add files

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,19 +1,23 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 
 
 class Base:
     # The above class is a base class.
 
-    __nb_objects = 0 # private attribute
-    
+    __nb_objects = 0  # private attribute
+
     def __init__(self, id=None):
         """
-        The function initializes an object with an ID, either provided as an argument or generated
+        The function initializes an object with an ID,
+        either provided as an argument or generated
         automatically.
-        
-        :param id: The "id" parameter is used to assign a unique identifier to an instance of the class.
-        If an "id" value is provided when creating an instance, it will be assigned to the instance's
-        "id" attribute. If no "id" value is provided, a unique identifier will be generated
+
+        :param id: The "id" parameter is used to assign
+        a unique identifier to an instance of the class.
+        If an "id" value is provided when creating an
+        instance, it will be assigned to the instance's
+        "id" attribute. If no "id" value is provided,
+        a unique identifier will be generated
         """
         if id is not None:
             self.id = id
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -3,13 +3,11 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    
+
     def __init__(self, size, x=0, y=0, id=None):
 
         super().__init__(size, size, id, x, y)
         self.size = size
-        
-        
+
     def __str__(self):
         return f'[Square] ({self.id}) {self.x}/{self.y} - {self.size}'
-            
