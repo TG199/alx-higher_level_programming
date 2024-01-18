775aff991e27eeb463587236f4b14dbd3db943a5
Kelechi <ebirikele@gmail.com>
2024-01-19 00:42:26 +0100

    change code structure

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-if __nam__ == "__main__":
+if __name__ == "__main__":
     import sys
 
-    if len(sys.argv) == 1:
-        print("0 arguments")
-    elif len(sys.argv) == 2:
-        print("1 argument:")
-        print(f"1: {sys.argv[1]}")
+    total_args = len(sys.argv) - 1
+    if total_args == 0:
+        print("{} arguments".format(total_args))
+    elif total_args == 1:
+        print("{} argument:".format(total_args))
     else:
-        Sum = 1
-        total_args = len(sys.argv) - 1
-        print(f"{total_args} arguments:")
-        for i in range(1, len(sys.argv)):
-            if total_args > 0:
-                print(f"{Sum}: {sys.argv[i]}")
-                Sum += 1
+        print("{} arguments:".format(total_args))
+    if total_args >= 1:
+        total_args = 0
+        for arg in sys.argv:
+            if total_args != 0:
+                print("{}: {}".format(total_args, arg))
+            total_args += 1
