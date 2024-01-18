33885acd66b8488b5d9a6eb2c6875a24d3396f14
Kelechi <ebirikele@gmail.com>
2024-01-19 00:27:09 +0100

    make file unexecutable when imorted

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-import sys
+if __nam__ == "__main__":
+    import sys
 
-if len(sys.argv) == 1:
-    print("0 arguments")
-elif len(sys.argv) == 2:
-    print("1 argument:")
-    print(f"1: {sys.argv[1]}")
-else:
-    Sum = 1
-    total_args = len(sys.argv) - 1
-    print(f"{total_args} arguments:")
-    for i in range(1, len(sys.argv)):
-        if total_args > 0:
-            print(f"{Sum}: {sys.argv[i]}")
-            Sum += 1
+    if len(sys.argv) == 1:
+        print("0 arguments")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+        print(f"1: {sys.argv[1]}")
+    else:
+        Sum = 1
+        total_args = len(sys.argv) - 1
+        print(f"{total_args} arguments:")
+        for i in range(1, len(sys.argv)):
+            if total_args > 0:
+                print(f"{Sum}: {sys.argv[i]}")
+                Sum += 1
