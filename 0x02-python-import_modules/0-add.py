700850239c44cadbf6bc861d2638b8f84793e100
Kelechi <ebirikele@gmail.com>
2024-01-18 21:53:58 +0100

    change structure of code

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-import add_0
-""" add function """
-
 if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add_0.add(a, b)), end="\n")
+    print("{} + {} = {}".format(a, b, add(a, b)))
