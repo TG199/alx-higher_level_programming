0e8b34e846c124015713c396d28f8daad799176f
Kelechi <ebirikele@gmail.com>
2024-01-18 21:26:08 +0100

    Add newline

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,4 +5,4 @@ import add_0
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+    print("{} + {} = {}".format(a, b, add_0.add(a, b)), end="\n")
