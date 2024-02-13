4a4b856f2b8b76386f30c889feff08cfc2390d63
Kelechi <ebirikele@gmail.com>
2024-02-13 01:30:06 +0100

    Write a class Rectangle that defines a rectangle by: (based on 7-rectangle.py)

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -137,12 +137,12 @@ class Rectangle:
             rect_2(Rectangle): second rectangle object
 
         Returns: rect_1 if >= rect_2 else returns rect_2
-        """      
+        """
         if not isinstance(rect_1, Rectangle):
             raise TypeError('rect_1 must be an instance of Rectangle')
         if not isinstance(rect_2, Rectangle):
             raise TypeError('rect_2 must be an instance of Rectangle')
         if rect_1.area() > rect_2.area():
-            return rect_1.area()
+            return rect_1
         else:
-            return rect_2.area()
+            return rect_2
