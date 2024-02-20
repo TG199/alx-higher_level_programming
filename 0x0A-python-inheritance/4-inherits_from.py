229a0affaf4cd0940109646e5324925b84410909
Kelechi <ebirikele@gmail.com>
2024-02-20 03:22:05 +0100

    Write a class Square that inherits from Rectangle (9-rectangle.py). (task based on 10-square.py).

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def inherits_from(obj, a_class):
-
+    """ Return True if obj is an instance of a class
+    derived from a_class
+    """
     if type(obj) is a_class:
         return False
     return isinstance(obj, a_class)
