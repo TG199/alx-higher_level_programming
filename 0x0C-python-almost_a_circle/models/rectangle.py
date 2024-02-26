342c3efb82ffd612be8c4b65a849f1bc41144091
Kelechi <ebirikele@gmail.com>
2024-02-27 00:29:05 +0100

    Add rectangle and square

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,242 +1,133 @@
 #!/usr/bin/python3
-
-"""Rectangle class module"""
-
-
+""" A Rectangle module
+"""
 from models.base import Base
+from models.validator import int_validator
 
 
 class Rectangle(Base):
-    """ The Rectangle class is a subclass of the Base class."""
+    """Represents a rectangle class """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """ Initializes all instance and base class attributes
+        Args:
+            width(int): width of rectangle
+            height(int): height of rectangle
+            x(int): x
+            y(int): y
+            id(int): id of instance
         """
-        The function initializes an object with specified
-        width, height, x and y coordinates.
-
-        :param width: The width parameter represents the
-        width of an object. It is used to define the
-        width of an object in a class or function
-        :param height: The height parameter represents
-        the height of an object. It is used to set the
-        height of the object when it is initialized
-        :param x: The x-coordinate of the top-left
-        corner of the object's bounding box. It represents
-        the horizontal position of the object on the screen
-        or canvas, defaults to 0 (optional)
-        :param y: The `y` parameter represents the
-        y-coordinate of the object's position. It determines
-        the vertical position of the object on a coordinate
-        plane, defaults to 0 (optional)
-        :param id: The `id` parameter is used to assign
-        a unique identifier to an object. It is an
-        optional parameter and if not provided, it will
-        be set to `None`
-        """
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
         super().__init__(id)
+        int_validator("width", width)
+        self.__width = width
+        int_validator("height", height)
+        self.__height = height
+        int_validator("x", x)
+        self.__x = x
+        int_validator("y", y)
+        self.__y = y
 
     @property
     def width(self):
-        """
-        The function returns the width attribute of an object.
-        :return: The width of the object.
+        """Gets Value of the width of rectangle
+        Returns: Width value
         """
         return self.__width
 
     @width.setter
-    def width(self, width):
+    def width(self, value):
+        """ Sets the value of width
+        Args:
+            value(int): value to set width
         """
-        The function sets the width attribute of an object
-        to the provided value, after performing type
-        and value checks.
-
-        :param width: The "width" parameter is used to set
-        the width of an object. It is expected to be
-        an integer value greater than zero
-        """
-        if type(width) is not int:
-            raise TypeError("width must be an integer")
-        if width <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = width
+        int_validator("width", value)
+        self.__width = value
 
     @property
     def height(self):
-        """
-        The function returns the height attribute of an object.
-        :return: The height of the object.
+        """Gets the value of height of rectangle
+        Returns: height value
         """
         return self.__height
 
     @height.setter
-    def height(self, height):
+    def height(self, value):
+        """ Sets the height value
+        Args:
+            value(int): value of height
         """
-        The function sets the height attribute of an object,
-        but raises an error if the input is not an
-        integer or if it is less than or equal to zero.
-
-        :param height: The parameter "height" is the value
-        that will be assigned to the instance
-        variable "__height". It is expected to be an integer.
-        If it is not an integer, a TypeError will
-        be raised. If it is less than or equal to 0, a
-        ValueError will be raised. Otherwise, the
-        """
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        if height <= 0:
-            raise ValueError("height must be > 0")
-
-        self.__height = height
+        int_validator("height", value)
+        self.__height = value
 
     @property
     def x(self):
-        """
-        The function returns the value of the private
-        variable __x.
-        :return: The method is returning the value of
-        the attribute `__x`.
+        """ Gets the value of x
+        Returns: the value of x
         """
         return self.__x
 
     @x.setter
-    def x(self, x):
+    def x(self, value):
+        """ Sets the value of x
+        Args:
+            value: value to set x to
         """
-        The function sets the value of a private variable __x
-        to the input x, but raises a ValueError if
-        x is less than 0.
-
-        :param x: The parameter "x" is an integer value
-        that represents a value to be assigned to the
-        attribute "__x" of an object
-        """
-
-        if type(x) is not int:
-            raise TypeError("x must be an integer")
-        if x < 0:
-            raise ValueError("x must be >= 0")
-
-        self.__x = x
+        int_validator("x", value)
+        self.__x = value
 
     @property
     def y(self):
-        """
-        The function returns the value of the
-        private variable __y.
-        :return: The method is returning the value of the
-        private variable `__y`.
+        """Gets the value of y
+        Returns: the value of y
         """
         return self.__y
 
     @y.setter
-    def y(self, y):
+    def y(self, value):
+        """Sets the value of y
+        Args:
+            value: value to set y to
         """
-        The function sets the value of the private attribute
-        "__y" to the input parameter "y", but
-        raises a ValueError if "y" is less than 0.
-
-        :param y: The parameter "y" is a value that
-        represents a coordinate or position on a vertical
-        axis
-        """
-        ''' if type(y) is not int:
-            raise TypeError("y must be an integer")
-        if y < 0:
-            raise ValueError('y must be >= 0')'''
-        if type(y) is not int:
-            raise TypeError("y must be an integer")
-        if y < 0:
-            raise ValueError("y must be >= 0")
-
-        self.__y = y
+        int_validator("y", value)
+        self.__y = value
 
     def area(self):
+        """ Area of the rectangle instance
+        Returns: Area of rectangle instance
         """
-        The above function calculates the area of a rectangle.
-        :return: The area of the rectangle, which is calculated
-        by multiplying the width and height of
-        the rectangle.
-        """
-        return self.width * self.height
+        return self.__width * self.__height
 
     def display(self):
+        """ Prints the representation of instance in '#'
         """
-        The function displays a rectangle of a given
-        width and height at a specified position (x, y) on
-        the screen.
-        :return: nothing (None).
-        """
-
-        rectangle = self.y * "\n"
-        for i in range(self.height):
-            rectangle += (" " * self.x)
-            rectangle += ("#" * self.width) + "\n"
+        rectangle = self.__y * "\n"
+        for i in range(self.__height):
+            rectangle += (" " * self.__x)
+            rectangle += ("#" * self.__width) + "\n"
 
         print(rectangle, end='')
-        ''' if self.width == 0 or self.height == 0:
-            print("")
-            return
-
-        for i in range((self.y)):
-            for j in range(self.x):
-                print("", end="")
-            print()
-
-        for i in range(self.height):
-            for j in range(self.width):
-                print("#", end="")
-            print()'''
-
-    def update(self, *args, **kwargs):
-        """
-        The function updates the attributes of an
-        object using either positional arguments or keyword
-        arguments.
-        """
-        if len(args) != 0:
-            for i in range(len(args)):
-                setattr(self, ["id", "width", "height", "x", "y"][i], args[i])
-
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-
-    def to_dictionary(self):
-        """
-        The function `to_dictionary` returns a dictionary containing
-        the attributes of an object.
-
-        Returns:
-          a dictionary with the following key-value pairs:
-        - 'id': the value of self.id
-        - 'width': the value of self.width
-        - 'height': the value of self.height
-        - 'x': the value of self.x
-        - 'y': the value of self.y
-        """
-        return {
-            'id': self.id,
-            'width': self.width,
-            'height': self.height,
-            'x': self.x,
-            'y': self.y
-        }
 
     def __str__(self):
+        """Prints class rep
         """
-        The above function returns a string representation
-        of a Rectangle object.
-        :return: The `__str__` method is returning a
-        string representation of a Rectangle object. The
-        string includes the object's id, x and y coordinates,
-        width, and height.
-        """
-        str_rectangle = "[Rectangle] "
-        str_id = "({}) ".format(self.id)
-        str_xy = "{}/{} - ".format(self.x, self.y)
-        str_wh = "{}/{}".format(self.width, self.height)
+        return f'[Rectangle] ({self.id}) {self.__x}/{self.__y} - '\
+            f'{self.__width}/{self.__height}'
 
-        return str_rectangle + str_id + str_xy + str_wh
+    def update(self, *args, **kwargs):
+        """Update method"""
+        if args is not None and len(args) != 0:
+            list_attr = ['id', 'width', 'height', 'x', 'y']
+            for i in range(len(args)):
+                setattr(self, list_attr[i], args[i])
+        else:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+
+    def to_dictionary(self):
+        return {
+            'id': self.id,
+            'x': self.x,
+            'y': self.y,
+            'width': self.width,
+            'height': self.height
+            }
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,105 +1,49 @@
 #!/usr/bin/python3
-
-"""Square class module"""
-
-
+""" A Square module
+"""
 from models.rectangle import Rectangle
+from models.validator import int_validator
 
 
 class Square(Rectangle):
-    """ The class Square is a subclass of the class Rectangle."""
+    """Represents a Square class"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        The above function is a constructor that initializes an
-        object with a given size, position, and
-        id.
-
-        Args:
-          size: The size parameter represents the width and height
-          of an object. It is used to determine
-          the dimensions of the object.
-          x: The x-coordinate of the object's position. It represents the
-          horizontal position of the
-        object on a 2D plane. By default, it is set to 0. Defaults to 0
-          y: The `y` parameter represents the y-coordinate of the object's
-          position. It determines the
-        vertical position of the object on a coordinate plane. Defaults to 0
-          id: The `id` parameter is an optional parameter that represents
-          the unique identifier for an
-        object. It is used to distinguish between different instances of
-        the same class. If no `id` is
-        provided, it will default to `None`.
-        """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """
-        The function returns the width of an object.
-
-        Returns:
-          The width of the object.
-        """
         return self.width
-
+    
     @size.setter
-    def size(self, size):
-        """
-        The function sets the width and height of an object to a given size.
-
-        Args:
-          size: The parameter "size" is the desired size for both the width
-          and height of an object.
-        """
-        self.width = size
-        self.height = size
+    def size(self, value):
+        int_validator("width", value)
+        self.width = value
+        int_validator("height", value)
+        self.height = value
 
     def update(self, *args, **kwargs):
-        """
-        The function updates the attributes of an object using either
-        positional arguments or keyword
-        arguments.
-        """
-        if len(args) != 0:
+        if args is not None and len(args) != 0:
+            list_attr = ['id', 'size', 'x', 'y']
             for i in range(len(args)):
-                setattr(self, ["id", "size", "x", "y"][i], args[i])
+                setattr(self, list_attr[i], args[i])
 
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        else:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
 
     def to_dictionary(self):
-        """
-        The function `to_dictionary` returns a dictionary containing
-        the attributes `id`, `size`, `x`,
-        and `y` of an object.
-
-        Returns:
-          a dictionary with the keys 'id', 'size', 'x', and 'y'.
-          The values of these keys are the
-        corresponding attributes of the object that the method is called on.
-        """
         return {
             'id': self.id,
-            'size': self.size,
+            'size': self.width,
             'x': self.x,
             'y': self.y
-        }
+            }
+
+        
 
     def __str__(self):
-        """
-        The __str__ function returns a string representation of a
-        Square object, including its id,
-        coordinates, and width.
+        return f'[Square] ({self.id}) {self.x}/{self.y} - {self.width}'
 
-        Returns:
-          The `__str__` method is returning a formatted string
-          that represents the Square object. It
-        includes the id, x and y coordinates, and the width of the square.
-        """
-        str_square = "[Square] "
-        str_id = "({}) ".format(self.id)
-        str_xy = "{}/{} - ".format(self.x, self.y)
-        str_wh = "{}".format(self.width)
 
-        return str_square + str_id + str_xy + str_wh
+
