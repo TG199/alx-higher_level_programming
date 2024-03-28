7b5d621adff1e06fcc2fdfa84e1cc7651e81fdc1
Kelechi <ebirikele@gmail.com>
2024-03-28 04:05:08 +0100

    TV shows

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+--  lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT tv_show.title, tv_shows_genres.genre_id
+FROM tv_shows
+JOIN tv_shows_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_shows_genres.genre_id;
