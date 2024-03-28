f00a2ca100d01ddc97a1e97f13835aa2071f9728
Kelechi <ebirikele@gmail.com>
2024-03-28 04:00:43 +0100

    Join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 --lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name AS city_name, states.name AS state_name-- Query
-FROM cities
-JOIN states ON cities.state_id = states.id;
+SELECT cities.id, cities.name, states.name -- Query
+FROM hbtn_0d_usa.cities
+JOIN hbtn_0d_usa.states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
