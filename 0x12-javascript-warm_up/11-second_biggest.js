0d59cec7b4d29e8c3d2a2b0ac1708ac374708be2
Kelechi <ebirikele@gmail.com>
2024-04-24 23:06:19 +0100

    Write a script that searches the second biggest integer in the list of arguments.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,21 +1,26 @@
 #!/usr/bin/node
 
-if (process.argv[2] === undefined || process.argv[3] === undefined) {
+if (process.argv.length < 4) {
   console.log(0);
-} else {
-  let max1 = parseInt(process.argv[2]);
-  let max2 = parseInt(process.argv[3]);
+  process.exit(0);
+}
+let max1 = parseInt(process.argv[2]);
+let max2 = parseInt(process.argv[3]);
 
-  if (max1 < max2) {
-    const a = max1;
-    max1 = max2;
-    max2 = a;
-  }
-  for (let i = 2; i < process.argv.length; i++) {
-    if (process.argv[i] > max1) {
+if (max1 < max2) {
+  const a = max1;
+  max1 = max2;
+  max2 = a;
+}
+for (let i = 4; i < process.argv.length; i++) {
+  const num = parseInt(process.argv[i]);
+  if (!isNaN(num)) {
+    if (num > max1) {
       max2 = max1;
-      max1 = process.argv[i];
+      max1 = num;
+    } else if (num > max2) {
+      max2 = num;
     }
   }
-  console.log(max2);
 }
+console.log(max2);
