b23e4b799d9d8a484527ff21df889d29537ec063
Kelechi <ebirikele@gmail.com>
2024-04-25 00:29:58 +0100

    Write a class Square that defines a square and inherits from Square of 5-square.js:

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -8,7 +8,7 @@ class Square extends SquareP {
     }
     for (let i = 0; i < this.height; i++) {
       let row = '';
-      for (let j = 0; j < this.wdith; j++) {
+      for (let j = 0; j < this.width; j++) {
         row += c;
       }
       console.log(row);
