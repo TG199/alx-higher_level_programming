b9d616540c3107909a1a761502759511f70d7529
Kelechi <ebirikele@gmail.com>
2024-04-29 00:23:57 +0100

    Write a script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     cursor = db.cursor()
     cursor.execute("SELECT cities.id, cities.name, states.name \
     FROM cities JOIN states ON cities.state_id = states.id;")
-    states = curorfetchall()
+    states = cursor.fetchall()
 
     for state in states:
         print(state)
