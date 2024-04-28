4da9b4fe9b81a00a134d3a24ac91564ac87eb6a0
Kelechi <ebirikele@gmail.com>
2024-04-29 00:19:55 +0100

    Write a script that lists all cities from the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Write a script that lists all cities from the database
+hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states ON cities.state_id = states.id;")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
