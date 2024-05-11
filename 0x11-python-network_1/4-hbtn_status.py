90591523abeeff265631f2e3b8d9ef2e806b51bb
Kelechi <ebirikele@gmail.com>
2024-05-11 01:46:56 +0100

    Write a Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     url = requests.get('https://alx-intranet.hbtn.io/status')
 
     type_ = url.text
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(t), t)
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type_), type_)
