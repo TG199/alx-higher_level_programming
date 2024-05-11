075b16982228a6d6c9b56e005dd03f83561fc085
Kelechi <ebirikele@gmail.com>
2024-05-11 01:45:37 +0100

    Write a Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Write a Python script that fetches
+https://alx-intranet.hbtn.io/status
+"""
+
+import requests
+
+if __name__ == '__main__':
+
+    url = requests.get('https://alx-intranet.hbtn.io/status')
+
+    type_ = url.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(t), t)
